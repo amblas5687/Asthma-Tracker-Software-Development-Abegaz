cc859b5d6c5c2dba009b96e8f365e9a61cdd273d
dhsmit6900 <dhsmit6900@ung.edu>
2016-11-09 19:17:27 -0500

    finished
    i tested it and everything works. if you test it and that is not the
    case please tell me. Christian, I only changed the account,
    asthmacontroller, and createaccount pages. however you will be better
    off copying and pasting the asthma controller with the amount of changes
    i made. amber said my gui is the old one so just make sure the my names
    line up with the new page

--- Asthma Tracker/src/model/Account.java
+++ Asthma Tracker/src/model/Account.java
@@ -1,7 +1,7 @@
 package model;
 
 public class Account {
-	private String firstName, lastName, userName, password, conPassword;
+	private String firstName, lastName, userName, password, conPassword, birthDate, fullName, relation, phone, email;
 	
 	public String getfirstName() {
 		return firstName;
@@ -32,5 +32,36 @@ public class Account {
 	}
 	public void setconPassword(String conPassword) {
 		this.conPassword = conPassword;
+	}
+	
+	public String getbirthDate() {
+		return birthDate;
+	}
+	public void setbirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getfullName() {
+		return fullName;
+	}
+	public void setfullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getrelation() {
+		return relation;
+	}
+	public void setrelation(String relation) {
+		this.relation = relation;
+	}
+	public String getphone() {
+		return phone;
+	}
+	public void setphone(String phone) {
+		this.phone = phone;
+	}
+	public String getemail() {
+		return email;
+	}
+	public void setemail(String email) {
+		this.email = email;
 	}
 }
