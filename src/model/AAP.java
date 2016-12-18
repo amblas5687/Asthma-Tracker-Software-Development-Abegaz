b61f8a6e32507d93c1c7be6d6c0b591c91872046
Anna Blasingame <amblas5687@ung.edu>
2016-12-17 22:31:10 -0500

    Fixed layout of file structure

---
+++ src/model/AAP.java
@@ -0,0 +1,142 @@
+//Anna
+package model;
+
+
+public class AAP 
+{
+	 
+		//mildMed
+	    private String mildMed;
+	    private String mildAmt;
+	    private String mildFreq;
+
+	    //modMed
+	    private String modMed;
+	    private String modAmt;
+	    private String modFreq;
+
+	    //sevMed
+	    private String sevMed;
+	    private String sevAmt;
+	    private String sevFreq;
+		
+	    //dr info
+	    private String drName;
+	    private String drPhone;
+	    private String drCity;
+	    
+	    
+	    public String getMildMed() {
+			return mildMed;
+		}
+	    
+		public void setMildMed(String mildMed) {
+			this.mildMed = mildMed;
+		}
+		
+		public String getMildAmt() {
+			return mildAmt;
+		}
+		
+		public void setMildAmt(String mildAmt) {
+			this.mildAmt = mildAmt;
+		}
+		
+		public String getMildFreq() {
+			return mildFreq;
+		}
+		
+		public void setMildFreq(String mildFreq) {
+			this.mildFreq = mildFreq;
+		}
+		
+		public String getModMed() {
+			return modMed;
+		}
+		
+		public void setModMed(String modMed) {
+			this.modMed = modMed;
+		}
+		
+		public String getModAmt() {
+			return modAmt;
+		}
+		
+		public void setModAmt(String modAmt) {
+			this.modAmt = modAmt;
+		}
+		
+		public String getModFreq() {
+			return modFreq;
+		}
+		
+		public void setModFreq(String modFreq) {
+			this.modFreq = modFreq;
+		}
+		
+		public String getSevMed() {
+			return sevMed;
+		}
+		
+		public void setSevMed(String sevMed) {
+			this.sevMed = sevMed;
+		}
+		
+		public String getSevAmt() {
+			return sevAmt;
+		}
+		
+		public void setSevAmt(String sevAmt) {
+			this.sevAmt = sevAmt;
+		}
+		
+		public String getSevFreq() {
+			return sevFreq;
+		}
+		
+		public void setSevFreq(String sevFreq) {
+			this.sevFreq = sevFreq;
+		}
+
+		public String getDrName() {
+			return drName;
+		}
+
+		public void setDrName(String drName) {
+			this.drName = drName;
+		}
+
+		public String getDrPhone() {
+			return drPhone;
+		}
+
+		public void setDrPhone(String drPhone) {
+			this.drPhone = drPhone;
+		}
+
+		public String getDrCity() {
+			return drCity;
+		}
+
+		public void setDrCity(String drCity) {
+			this.drCity = drCity;
+		}
+
+		@Override
+		public String toString() {
+			return "AAP [mildMed=" + mildMed + ", mildAmt=" + mildAmt + ", mildFreq=" + mildFreq + ", modMed=" + modMed
+					+ ", modAmt=" + modAmt + ", modFreq=" + modFreq + ", sevMed=" + sevMed + ", sevAmt=" + sevAmt
+					+ ", sevFreq=" + sevFreq + ", drName=" + drName + ", drPhone=" + drPhone + ", drCity=" + drCity
+					+ "]";
+		}
+		
+		
+		
+
+	
+	    
+		
+	    
+
+
+}
